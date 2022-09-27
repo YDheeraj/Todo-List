13472a977143242a31d5c1252362179a95a43da8
Dheeraj Kumar Yadav <yadavdheeraj2002@gmail.com>
2022-09-27 23:49:57 +0530

    changes added on 27-09-2022

--- src/component/Basics/Todo.js
+++ src/component/Basics/Todo.js
@@ -12,6 +12,11 @@ const TodoList=()=>{
             </div>
             <div className="image">
                 <img  src="./images/todo.png" alt="tod "/>
+                <figcaption>Add your list Here ✔️ </figcaption>
+            </div>
+            <div className="addItems">
+                <input type="text" placeholder="✍️ Add Items"></input>
+                <i class="fa-solid fa-2x fa-plus btn plus"></i>
             </div>
             
         </div>
