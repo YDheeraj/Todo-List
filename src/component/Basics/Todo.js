52af9f88849628bd9a47b77c2246b312054d2ef1
Dheeraj Kumar Yadav <yadavdheeraj2002@gmail.com>
2022-09-27 14:54:22 +0530

    Project Start

---
+++ src/component/Basics/Todo.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./style.css";
+
+const TodoList=()=>{
+
+
+    return (
+        <>
+        <div>
+            <div className="main-heading">
+                <h1>Todo-List</h1>
+            </div>
+            <div className="image">
+                <img  src="./images/todo.png" alt="tod "/>
+            </div>
+            
+        </div>
+        </>
+    )
+}
+
+export default TodoList;
