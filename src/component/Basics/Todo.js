394fbdc5bbb6aeecebfa4f17a01a4bc6602ef96f
Dheeraj Kumar Yadav <yadavdheeraj2002@gmail.com>
2022-09-28 23:18:50 +0530

    changes added on 28-09-2022

--- src/component/Basics/Todo.js
+++ src/component/Basics/Todo.js
@@ -18,6 +18,21 @@ const TodoList=()=>{
                 <input type="text" placeholder="✍️ Add Items"></input>
                 <i class="fa-solid fa-2x fa-plus btn plus"></i>
             </div>
+            <div className="show-Items">
+                <div className="eachItems">
+                    <h3>Apple</h3>
+                    <div className="todo-btn">
+                    <i class="fa-solid fa-2x fa-edit add-btn "></i>
+                    <i class="fa-solid fa-2x fa-trash add-btn"></i>
+                    </div>
+                </div>
+            </div>
+            
+            <div className="show-Items">
+                 <button className="main-btn">
+                    <span><b>CHECKLIST</b></span>
+                 </button>
+            </div>
             
         </div>
         </>
